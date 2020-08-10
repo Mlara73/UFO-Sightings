fe1482e0367833f77251bfea2acd3f8843e7abc5
Mlara <64501664+Mlara73@users.noreply.github.com>
2020-08-10 09:39:37 -0500

    inserting tableData dinamically with javascripy

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,20 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Table append
+// tbody reference
+
+var tbody = d3.select("tbody");
+
+// For Each Loop to iterate over the tableData array
+tableData.forEach(ufoItem => {
+    // console.log(ufoItem);
+    var row = tbody.append("tr");
+    
+    // iterate over each object value
+    Object.values(ufoItem).forEach(value => {
+        var cell = row.append("td");
+        cell.text(value)
+    });
+});
+
